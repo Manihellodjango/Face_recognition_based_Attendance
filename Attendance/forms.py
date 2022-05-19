ac446a644acbf9f99e85a5f535a592c3f1a819ef
manikumaryadla <yadlapallimanikumar1@gmail.com>
2022-05-19 16:07:58 +0200

    second commit

--- Attendance/forms.py
+++ Attendance/forms.py
@@ -4,26 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
 
-
-
 class DateInput(forms.DateInput):
     input_type = 'date'
 class TimeInput(forms.TimeInput):
     input_type = 'time'
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField() 
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model =User
-        fields = ['username','password1','password2']
-
-    def __init__(self,*args,**kwargs):
-        super(CustomUserCreationForm, self).__init__(*args,**kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-
 
 class ProfileForm(forms.ModelForm):
     class Meta:
